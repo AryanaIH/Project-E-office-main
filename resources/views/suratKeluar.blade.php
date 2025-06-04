3a07118b21c3bbed48a63950d3382a0295c4bae4
AryanaIH <125250537+AryanaIH@users.noreply.github.com>
2025-06-04 11:01:42 +0700

    Update suratKeluar.blade.php

--- resources/views/suratKeluar.blade.php
+++ resources/views/suratKeluar.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Daftar Surat Keluar</title>
+    <title>Daftar Surat Keluar Kampus 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body, html {
